41196faf6ff65478ffa922f547f6fd3b1191d1f1
SanchitBanati33 <86164665+SanchitBanati33@users.noreply.github.com>
2022-04-20 02:50:16 +0530

    Start Image Componenet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Circle from './components/Circle';
 import FAQ from './components/FAQ';
 import Footer from './components/Footer';
 import Speakers from './components/Speakers';
@@ -8,7 +9,9 @@ import Sponsers from './components/Sponsers';
 function App() {
   return (
     <div className="App">
+      <Circle></Circle>
       <Sponsers></Sponsers>
+
       <Footer></Footer>
       <FAQ></FAQ>
  
---
+++ src/components/Circle/index.js
@@ -0,0 +1,67 @@
+import React from 'react'; 
+import styled from "styled-components"; 
+import Header from "../../assets/circle.png";
+import Star from "../../assets/star.png";
+
+
+const Container = styled.div`
+background: #354B37;
+`;
+
+const ImgContainer = styled.div`
+
+@media (max-width: 700px) {
+
+  }
+`;
+
+const TextContainer = styled.div`
+padding:10%;
+@media (max-width: 700px) {
+    padding:1%;
+}
+
+`;
+
+const Title = styled.div`
+font-family: 'Dahlia';
+font-style: normal;
+font-weight: 400;
+font-size: 96px;
+line-height: 100px;
+/* or 104% */
+text-align: center;
+color: #F5C34B;
+
+@media (max-width: 700px) {
+    font-family: 'Dahlia';
+    font-style: normal;
+    font-size:50px;
+    line-height:56px;
+    padding: 10% 5% 10% 5%;
+    text-align: center;
+    color: #F5C34B;
+    margin-bottom:10%;
+    
+}
+`;
+
+const Circle = () => {
+  return (
+    <>
+    <Container>
+        <ImgContainer>
+        <img src={Header} width="100%"></img>
+            
+        </ImgContainer>
+        <TextContainer>
+            <Title>
+            Exploring how Blockchain Technology can help build a  sustainable world full of Human Potential
+            </Title>
+        </TextContainer>
+    </Container>
+    </>
+  )
+}
+
+export default Circle
