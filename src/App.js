b1b63f75a940e03721d803e0a95d9d6358f54237
DishaAgarwalla <dishaagarwalla2001@gmail.com>
2022-04-26 23:44:25 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./App.css";
 import Circle from "./components/Circle";
 import FAQ from "./components/FAQ";
@@ -7,42 +6,18 @@ import Speakers from "./components/Speakers";
 import Sponsers from "./components/Sponsers";
 import Intro from "./components/Intro";
 import Hero from "./components/Dual";
+import Navbars from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbars></Navbars>
       <Intro></Intro>
       <Circle></Circle>
-      <Hero></Hero>
-      <Sponsers></Sponsers>
+      {/* <Hero></Hero> */}
       <FAQ></FAQ>
       <Footer></Footer>
     </div>
-=======
-
-import './App.css';
-import Circle from './components/Circle';
-import FAQ from './components/FAQ';
-import Footer from './components/Footer';
-import Speakers from './components/Speakers';
-import Sponsers from './components/Sponsers';
-import Intro from "./components/Intro"
-import Hero from './components/Dual';
-import Navbars from "./components/Navbar";
-
-
-
-function App() {
-  return (
-  <div className="App">
-    <Navbars></Navbars>
-  <Intro></Intro>
-<Circle></Circle>
-{/* <Hero></Hero> */}
-<FAQ></FAQ>
-<Footer></Footer>
-  </div>
->>>>>>> 34724b5c6d5c9aae3dccc610206680e47c89b827
   );
 }
 
