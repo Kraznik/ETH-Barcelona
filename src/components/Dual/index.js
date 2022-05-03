a95f1c3a50fd42b50236ca2a29597a8ff04a55f8
SanchitBanati33 <86164665+SanchitBanati33@users.noreply.github.com>
2022-05-04 01:01:34 +0530

    Revert "Conflicts"
    This reverts commit 31d76f672a62c4023908f792c1c5855cacfa438b.

--- src/components/Dual/index.js
+++ src/components/Dual/index.js
@@ -5,92 +5,33 @@ import Event2 from "../../assets/Event2.png";
 import Ticket1 from "../../assets/Ticket1.png";
 import Ticket2 from "../../assets/Ticket2.png";
 
-import EventMobile1 from "../../assets/MobileImageBack1.png"
-import EventMobile2 from "../../assets/MobileImageFront1.png"
-import TicketMobile1 from "../../assets/MobileImageBack2.png"
-import TicketMobile2 from "../../assets/MobileImageFront2.png"
 import { useNavigate } from "react-router-dom";
 
-
-import { Scrollchor } from "react-scrollchor";
-
-import "./style.css";
+// import ScrollableSection, { ScrollableLink } from "react-update-url-on-scroll";
 
 const Container = styled.div`
   background: #f4f4f5;
-  overflow-y: auto;
-  height: 900px;
-  position: relative;
 `;
-
 const Right = styled.div`
   width: 646px;
   display: inline-block;
   float: right;
-  position: relative;
-`;
-/* background-image: url(${imageUrlOut}); */
-
-const OuterContainer = styled.div`
-  /* background-image: url(${Event1}); */
-  height: 900px;
-  padding: 15%;
-  top: 0px;
-  right: 0px;
-  position: sticky;
-  margin-top: -68px;
-  z-index: 100 !important;
-`;
-
-const ImageRight = styled.img`
-  height: 800px;
-  /* padding: 15%; */
-  /* top: 0px;
-  right: 0px;
-  position: sticky; */
-`;
-
-/* background-image: url(${imageUrlIn}); */
-
-const ImageContainer = styled.div`
-  /* background-image: url(${Event2}); */
-  height: 613px;
 `;
 
 const OuterContainer2 = styled.div`
-  /* background-image: url(${Ticket1}); */
+  background-image: url(${Ticket1});
   height: 900px;
   padding: 15%;
-  z-index: 1000 !important;
-  position: sticky;
-  margin-top: 68px;
-
-  @media (max-width: 800px) {
-    width: 375px;
-    background-image: url(${TicketMobile1});
-height: 522px;
-  }
 `;
 const TextContainer = styled.div`
   width: 518px;
   padding: 5%;
   background: #f4f4f5; ;
-
-    @media (max-width: 800px) {
-      width: 300.7px;
-      height: 419.34px;
-  }
 `;
 
 const ImageContainer2 = styled.div`
-  /* background-image: url(${Ticket2}); */
+  background-image: url(${Ticket2});
   height: 613px;
-
-  @media (max-width: 800px) {
-    width: 272.83px;
-    height: 355.54px;
-    background-image: url(${TicketMobile2});
-  }
 `;
 
 const Heading = styled.div`
@@ -102,73 +43,42 @@ const Heading = styled.div`
   text-align: center;
   color: #424242;
   padding: 2%;
+
   margin-top: 5%;
 `;
 
 const Left = styled.div`
   display: inline-block;
   height: 900px;
-  /* position: relative; */
-`;
-
-const Navbar = styled.div`
-  background: #424242;
-  height: 48px;
-  width: 300px;
-  margin: 20px 0% 0% 0%;
-  border-radius: 100px;
-  top: 20px;
-  left: 100px;
-  position: sticky;
-  z-index: 20;
-
-  a {
-    text-decoration: none;
-    color: white;
-  }
-`
-
-const Title3 = styled.div`
-  font-family: "Dahlia-bold";
-  font-style: normal;
-  font-weight: 700;
-  font-size: 78px;
-  text-align: left;
-  line-height: 78px;
-  margin-left:48%;
-  /* or 100% */
-  letter-spacing: -0.02em;
-
-  color: #424242;
-
-
-  @media (max-width: 800px) {
-    margin-left:20%;
-     }
+  /* overflow-y: auto; */
+  position: relative;
 `;
 
 const Title2 = styled.div`
-  font-family: "Dahlia";
+  font-family: "Dahlia-normal";
   font-style: normal;
   font-weight: 700;
   font-size: 78px;
   text-align: right;
-  font-weight: 400;
-  font-size: 78px;
-  padding: 0%;
-  width: 1000px;
-  text-align: left;
   line-height: 78px;
-  color: #424242;
-  margin-left:10%;
+  /* or 100% */
+  letter-spacing: -0.02em;
 
-  @media (max-width: 800px) {
-    margin-left:10%;
-    font-size:56px;
-    text-align:left;
-    width:343px;
-    font-family: "Dahlia-normal";
-     }
+  color: #424242;
+`;
+
+const Title3 = styled.div`
+  font-family: "Dahlia-normal";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 58px;
+  text-align: right;
+  line-height: 78px;
+  /* or 100% */
+
+  letter-spacing: -0.02em;
+
+  color: #424242;
 `;
 
 const Title = styled.div`
@@ -178,17 +88,9 @@ const Title = styled.div`
   font-size: 78px;
   padding: 0%;
   width: 700px;
-  text-align: right;
+  text-align: center;
   line-height: 78px;
   color: #424242;
-
-  @media (max-width: 800px) {
-    margin-left:10%;
-    font-size:50px;
-    width: 100%;
-    text-align:left;
-
-     }
 `;
 
 const Description = styled.div`
@@ -197,6 +99,7 @@ const Description = styled.div`
   font-weight: 400;
   font-size: 16px;
   line-height: 20px;
+  /* or 125% */
   text-align: left;
   display: flex;
   align-items: left;
@@ -204,85 +107,23 @@ const Description = styled.div`
   color: #424242;
 
   color: #424242;
-  width: 300px;
-  margin: -5% 0 0 20%;
+
+  margin: 0% 0 0 43%;
 `;
 
 const TextBox = styled.div`
-  margin: 30% 10% 10% 0%;
-
-  margin: 0% 0 0 43%;
-
-  @media (max-width: 800px) {
-   margin-left:10%
-   width: 375px;
-  
-    }
-
-  @media (max-width: 900px) {
-    margin-left:10% 
-     width: 375px;
-     
-   }
-
-       @media (max-width: 700px) {
-        margin-left:10%
-        width: 375px;
-       
-         }
-
-
-`;
-
-const TextBox2 = styled.div`
   margin: 10% 10% 10% 0%;
-
-  @media (max-width: 800px) {
-padding-top:35%;
-font-size: 56px;
-}
-  
 `;
-
-const Description2 = styled.div`
-font-family: "Montserrat";
-font-style: normal;
-font-weight: 400;
-width: 412px;
-font-size: 16px;
-line-height: 20px;
-text-align: left;
-display: flex;
-align-items: left;
-
-color: #424242;
-
-margin: -5% 0 0 20%;
-
-
-
-  @media (max-width: 800px) {
-    padding-bottom:35%;
-    margin-left:10%;
-    padding-right:20%;
-    width: 375px;
-    }
-
-
-`;
-
-
 
 const Button = styled.div`
   border: 0.8px solid #354b37;
   box-sizing: border-box;
-  transform: rotate(-6.41deg);
   font-family: "Dahlia";
   font-style: normal;
   font-weight: 700;
   font-size: 20px;
   text-align: center;
-  margin: 5% 5% 5% 18%;
+  margin: 5% 5% 5% 45%;
   line-height: 20px;
   justify-content: center;
   border-radius: 50%;
@@ -294,51 +135,25 @@ const Button = styled.div`
   text-align: center;
   width: 100px;
   color: #354b37;
-
-  @media (max-width: 800px) {
-    margin-bottom:35%;
-    margin-left:35%;
-    margin-top:10%;
-
-
-    }
 `;
 
 const SectionContainer = styled.div`
-  height: 900px;
+  width: 700px;
   display: grid;
   align-content: center;
-  padding-bottom: 200px;
+
+  @media (max-width: 800px) {
+    /* display: none; */
+  }
 `;
 
-const SectionContainer1 = styled(SectionContainer)`
-  /* top: 0px;
-  position: sticky; */
-`;
-const SectionContainer2 = styled(SectionContainer)`
-  /* position: sticky; */
-`;
+const SectionContainer1 = styled(SectionContainer)``;
+const SectionContainer2 = styled(SectionContainer)``;
 
-const onScroll = () => {
-  var currentHash = "#initial_hash";
-  document.scroll(function () {
-    ".anchor_tags".each(function () {
-      var top = window.pageYOffset;
-      var distance = top - this.offset().top;
-      var hash = this.attr("href");
-      // 30 is an arbitrary padding choice,
-      // if you want a precise check then use distance===0
-      if (distance < 30 && distance > -30 && currentHash != hash) {
-        window.location.hash = hash;
-        currentHash = hash;
-      }
-    });
-  });
-};
-
-const Hero = () => {
+const Hero = ({ isMobile }) => {
   const [imageUrlOut, setImageUrlOut] = useState("");
   const [imageUrlIn, setImageUrlIn] = useState("");
+  const [section2, setSection2] = useState(false);
 
   const location = window.location.hash;
   let navigate = useNavigate();
@@ -351,6 +166,7 @@ const Hero = () => {
     if (location.slice(-1) === "2") {
       setImageUrlOut(Ticket1);
       setImageUrlIn(Ticket2);
+      setSection2(true);
     }
   }, [location]);
 
@@ -383,6 +199,7 @@ const Hero = () => {
     @media (max-width: 800px) {
       display: none;
       position: none;
+      background: red;
     }
   `;
 
@@ -390,92 +207,51 @@ const Hero = () => {
     background-image: url(${imageUrlOut});
     height: 900px;
     padding: 15%;
-
-    @media (max-width: 800px) {
-      width: 375px;
-height: 522px;
-background-image: url(${EventMobile1});
-    }
-
   `;
 
   const ImageContainer = styled.div`
     background-image: url(${imageUrlIn});
     height: 613px;
-
-    @media (max-width: 800px) {
-      width: 272.83px;
-      height: 355.54px;
-      background-image: url(${EventMobile2});
-    }
   `;
   return (
     <>
       <Container>
-        <Navbar
-          style={{
-            display: "flex",
-            padding: "10px 50px",
-            gap: "40px",
-          }}
-        >
-
-          <Scrollchor to="#section1">Section1</Scrollchor>
-          <Scrollchor to="#section2">Section2</Scrollchor>
-        </Navbar>
-        <Right>
-
-          <OuterContainer className="outimg outimg1">
-            <TextContainer>
-              <ImageContainer className="inimg inimg1"></ImageContainer>
-              <Heading>Barcelona . Spain</Heading>
-            </TextContainer>
-          </OuterContainer>
-
-          <OuterContainer2 className="outimg outimg2">
-            <TextContainer>
-              <ImageContainer2 className="inimg inimg2"></ImageContainer2>
-              <a href="#claim">
-                <Heading>Claim your NFT</Heading>
-              </a>
-            </TextContainer>
-          </OuterContainer2>
-
-        </Right>
-
-        <Left>
-          <SectionContainer1 id="section1">
+        {isMobile ? (
+          <>
+            <OuterContainer>
+              <TextContainer>
+                <ImageContainer></ImageContainer>
+                <Heading>Barcelona . Spain</Heading>
+              </TextContainer>
+            </OuterContainer>
             <TextBox>
               <Title>Join us in sunny </Title>
               <Title2>Barcelona </Title2>
             </TextBox>
             <Description>
               We bring together over 4000 makers, developers, and blockchain{" "}
-              <br></br>enthusiasts for a three-day<br></br> conference. You can
-              learn from the<br></br> best in the crypto scene, and finally{" "}
-              <br></br> put those networking skills to use.
+              enthusiasts for a three-day<br></br> conference. You can learn
+              from the<br></br> best in the crypto scene, and finally <br></br>{" "}
+              put those networking skills to use.
             </Description>
             <Button>Join us</Button>{" "}
-          </SectionContainer1>
-
-          <SectionContainer2 id="section2">
+            <OuterContainer2>
+              <TextContainer>
+                <ImageContainer2></ImageContainer2>
+                <Heading>Claim your NFTickets</Heading>
+              </TextContainer>
+            </OuterContainer2>
             <TextBox>
-              <Title>NFTickets are going fast…</Title>
-              <Title>Claim your at</Title>
-              <Title2>DoinGud </Title2>
               <Title3>NFTickets are going fast…</Title3>
-              <Title3>Claim yours at </Title3><Title2>DoinGud</Title2>
+              <Title3>Claim your at DoinGud</Title3>
             </TextBox>
-            <Description2>
-              On the days leading up to the event, your NFTs will  be
-              redeemed to check-in on our website.<br></br><br></br>After, you will
-              receive a QR-code that grants you access to the
+            <Description>
+              On the days leading up to the event, your NFTs will <br></br> be
+              redeemed to check-in on our website.<br></br> After, you will
+              receive a QR-code that grants you <br></br>access to the
               ETHBarcelona conference.
-            </Description2>
-          </SectionContainer2>
-        </Left>
-
-
+            </Description>
+          </>
         ) : (
           <>
             <Right>
@@ -539,22 +315,21 @@ background-image: url(${EventMobile1});
               ) : (
                 <SectionContainer2 id="section2">
                   <TextBox>
-                    <Title3>NFTickets are going fast… <br></br>
-                 Claim your at DoinGud</Title3>
+                    <Title3>NFTickets are going fast…</Title3>
+                    <Title3>Claim your at DoinGud</Title3>
                   </TextBox>
-                  <Description2>
-                    On the days leading up to the event, your NFTs will
-                     be redeemed to check-in on our website.<br></br> <br></br>
-                    After, you will receive a QR-code that grants you 
+                  <Description>
+                    On the days leading up to the event, your NFTs will{" "}
+                    <br></br> be redeemed to check-in on our website.<br></br>{" "}
+                    After, you will receive a QR-code that grants you <br></br>
                     access to the ETHBarcelona conference.
-                  </Description2>
+                  </Description>
                 </SectionContainer2>
               )}
             </Left>
           </>
-
+        )}
       </Container>
-
     </>
   );
 };
