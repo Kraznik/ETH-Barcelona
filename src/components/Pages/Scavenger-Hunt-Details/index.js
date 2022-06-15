c915b39684bf7cb1c9e3cb816ef55e886b64bf6b
SanchitBanati33 <86164665+SanchitBanati33@users.noreply.github.com>
2022-06-15 17:09:35 +0530

    Heart Images

--- src/components/Pages/Scavenger-Hunt-Details/index.js
+++ src/components/Pages/Scavenger-Hunt-Details/index.js
@@ -3,12 +3,44 @@ import styled from "styled-components";
 import { Title1, Title2, Footer, Question } from "../Speaker-Cards";
 import Accordion from "react-bootstrap/Accordion";
 import "bootstrap/dist/css/bootstrap.css";
+import H1 from "../../../assets/Hunt1.svg";
+import H2 from "../../../assets/Hunt2.svg";
+import H3 from "../../../assets/Hunt3.svg";
+import H4 from "../../../assets/Hunt4.png";
+import H5 from "../../../assets/Hunt5.svg";
+import H6 from "../../../assets/Hunt6.svg";
+import H7 from "../../../assets/Hunt7.svg";
+import H8 from "../../../assets/Hunt8.svg";
+
+const Heart = styled.div``
+
+const LineContainer = styled.div``
+
 
 const ScavengerHuntDetails = () => {
   return (
     <>
       <Title1>DoGood</Title1>
       <Title2>SCAVENGER HUNT</Title2>
+
+      <Heart>
+        <LineContainer>
+            <img src={H1}></img>
+            <img src={H2}></img>
+            <img src={H3}></img>
+        </LineContainer>
+        <LineContainer>
+            <img src={H4}></img>
+            <img src={H5}></img>
+            <img src={H6}></img>
+        </LineContainer>
+        <LineContainer>
+        <img src={H7}></img>
+        <img src={H8}></img>
+        </LineContainer>
+
+      </Heart>
+
 
       <Accordion>
         <Accordion.Item eventKey="2" className="">
