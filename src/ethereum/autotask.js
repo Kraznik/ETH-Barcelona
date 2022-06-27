c0e572baa0f42053276818fd6aca12d981df99d8
DishaAgarwalla <dishaagarwalla2001@gmail.com>
2022-06-27 16:48:44 +0530

    autotask

--- src/ethereum/autotask.js
+++ src/ethereum/autotask.js
@@ -29,14 +29,19 @@ exports.main = async function (signer, recipient, payload) {
       block_number + 1
     }&key=ckey_9f2ed5152bcb4eb1a8dbc4cf854`;
 
+    console.log("covalent url: ", url_get);
+
     const { data } = await axios.get(url_get);
     const items = data.data.items;
     let token_id;
-    await items.map((item) => {
-      if (item.decoded) {
-        token_id = item.decoded.params[3].value;
-      }
-    });
+    while (typeof token_id === "undefined") {
+      await items.map((item) => {
+        if (item.decoded) {
+          token_id = item.decoded.params[3].value;
+        }
+      });
+    }
+
     // const token_id = data.data.items[0].decoded.params[3].value;
     console.log("token id: ", token_id);
 
